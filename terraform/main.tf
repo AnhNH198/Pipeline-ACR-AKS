df9d29be6fa52b0a4a87880c80ca5cabf00028bc
boploi <boploi198@gmail.com>
2021-04-12 12:23:55 +0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,17 +37,17 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled = false
 }
 
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_role_assignment" "role_acrpull" {
+#   scope = azurerm_container_registry.acr.id
+#   role_definition_name = "AcrPull"
+#   principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+#   skip_service_principal_aad_check = true
+# }
 
-resource "azurerm_role_assignment" "role_acrpush" {
-  scope = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPush"
-  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_role_assignment" "role_acrpush" {
+#   scope = azurerm_container_registry.acr.id
+#   role_definition_name = "AcrPush"
+#   principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+#   skip_service_principal_aad_check = true
+# }
 
